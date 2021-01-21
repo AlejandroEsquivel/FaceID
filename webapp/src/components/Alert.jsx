f5e326d039b151dc75917c54a5dbfeb5aaf321fa
Alejandro Esquivel <ae@alejandro.ltd>
2021-01-20 17:13:47 -0800

    added Alert component, updated yarn.lock with firebase module and material/lab module

---
+++ webapp/src/components/Alert.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import Snackbar from '@material-ui/core/Snackbar';
+import MuiAlert from '@material-ui/lab/Alert';
+import { makeStyles } from '@material-ui/core/styles';
+
+export default (props) => {
+  return <MuiAlert elevation={6} variant="filled" {...props} />;
+}
